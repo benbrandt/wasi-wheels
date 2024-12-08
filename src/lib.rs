81619c984f723ae882738ef3bacf272ec17fec57
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-12-08 22:26:17 +0100

    Use version for the key of the sdist files

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,36 @@
 mod tests {
     use std::collections::HashMap;
 
+    use heck::ToSnakeCase;
+    use itertools::Itertools;
     use reqwest::Client;
     use serde::Deserialize;
 
     /// Registry information for a given Python project in the registry
     #[derive(Deserialize)]
     struct Project {
+        name: String,
         files: Vec<ProjectFile>,
         versions: Vec<String>,
     }
 
     impl Project {
+        /// Only return the relevant project file for the source code of each release
         fn sdist_files(self) -> HashMap<String, ProjectFile> {
             self.files
                 .into_iter()
-                .filter(|file| file.filename.ends_with("tar.gz"))
-                .map(|file| (file.filename.clone(), file))
+                .filter_map(|file| {
+                    let filename = file.filename.rsplit_once(".tar.gz")?.0;
+                    let version = filename
+                        // Check for project name as is
+                        .split_once(&format!("{}-", self.name))
+                        .or_else(|| {
+                            // Also check for snakecase version
+                            filename.split_once(&format!("{}-", self.name.to_snake_case()))
+                        })?
+                        .1;
+                    Some((version.to_owned(), file))
+                })
                 .collect()
         }
     }
@@ -61,9 +75,16 @@ mod tests {
     async fn can_retrieve_sdist_files_from_pypi() -> anyhow::Result<()> {
         let index = PythonPackageIndex::new("https://pypi.org");
         let project_name = "pydantic-core";
-        let project = index.project(project_name).await?;
 
-        assert_eq!(project.versions.len(), project.sdist_files().len());
+        let project = index.project(project_name).await?;
+        let mut versions = project.versions.clone();
+        versions.sort();
+        let sdist_files = project.sdist_files();
+
+        // There is one file for every version
+        assert_eq!(versions.len(), sdist_files.len());
+        // The keys match the versions
+        assert_eq!(versions, sdist_files.into_keys().sorted().collect_vec());
 
         Ok(())
     }
