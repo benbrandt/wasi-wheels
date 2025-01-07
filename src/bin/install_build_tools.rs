1423d7385ddf7d3ac405382b4bbb63fa31850ed3
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-01-07 16:19:09 +0100

    refactor: merge install command into main binary

--- src/bin/install_build_tools.rs
+++
@@ -1,8 +0,0 @@
-//! Prep Cpython and WASI SDK tooling
-
-use wasi_wheels::download_and_compile_cpython;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    download_and_compile_cpython().await
-}
