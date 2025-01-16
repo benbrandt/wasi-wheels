5f56e62a0f4cc9d112f9cfbc45cadf9904879118
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-01-16 23:18:23 +0100

    refactor: use wasi.py for 3.13 builds

--- src/build/build_tools.rs
+++ src/build/build_tools.rs
@@ -45,6 +45,26 @@ impl PythonVersion {
         REPO_DIR.join(format!("cpython-{}", self.current_patch_version()))
     }
 
+    /// Directory for the wasi install directory
+    #[must_use]
+    pub fn cross_prefix(self) -> PathBuf {
+        self.wasi_dir().join("install")
+    }
+
+    /// Directory to find the lib files for wasi
+    #[must_use]
+    pub fn cross_lib_dir(self) -> PathBuf {
+        self.wasi_dir()
+            .join(format!("build/lib.wasi-wasm32-{self}"))
+    }
+
+    fn wasi_dir(self) -> PathBuf {
+        self.cpython_dir().join(match self {
+            PythonVersion::Py3_12 => "builddir/wasi",
+            PythonVersion::Py3_13 => "cross-build/wasm32-wasip2",
+        })
+    }
+
     /// Which GitHub branch should be used for downloading
     fn github_branch(self) -> String {
         format!("{}-wasi", self.current_patch_version())
@@ -58,6 +78,107 @@ impl PythonVersion {
     /// # Panics
     /// If certain paths are invalid because of failed download
     pub async fn download_and_compile_cpython(self) -> anyhow::Result<()> {
+        match self {
+            PythonVersion::Py3_12 => self.download_and_compile_legacy().await,
+            PythonVersion::Py3_13 => self.download_and_compile_with_wasi_script().await,
+        }
+    }
+
+    /// Downloads and compiles a fork of `CPython` that can be compiled to WASI for use with componentize-py
+    /// Uses the newer wasi.py script to do it, if available.
+    ///
+    /// # Errors
+    /// Will error if the repo cannot be downloaded or compilation fails
+    ///
+    /// # Panics
+    /// If certain paths are invalid because of failed download
+    async fn download_and_compile_with_wasi_script(self) -> anyhow::Result<()> {
+        #[cfg(any(target_os = "macos", target_os = "windows"))]
+        const PYTHON_EXECUTABLE: &str = "python.exe";
+        #[cfg(not(any(target_os = "macos", target_os = "windows")))]
+        const PYTHON_EXECUTABLE: &str = "python";
+        const HOST_TRIPLE: &str = "wasm32-wasip2";
+        let github_branch = self.github_branch();
+        let cpython = self.cpython_dir();
+
+        if !cpython.exists() {
+            let bytes = reqwest::get(format!(
+                "https://github.com/{}/{}/archive/refs/heads/{github_branch}.tar.gz",
+                Self::GITHUB_USER,
+                Self::GITHUB_REPO
+            ))
+            .await?
+            .error_for_status()?
+            .bytes()
+            .await?;
+            tokio::task::spawn_blocking(move || {
+                Archive::new(GzDecoder::new(&bytes[..])).unpack(REPO_DIR.as_path())
+            })
+            .await??;
+            fs::rename(format!("{}-{github_branch}", Self::GITHUB_REPO), &cpython).await?;
+        }
+
+        let cpython_wasi_dir = cpython.join(format!("cross-build/{HOST_TRIPLE}"));
+        let cpython_native_dir = cpython.join("cross-build/build");
+
+        if !cpython_wasi_dir.join(format!("libpython{self}.a")).exists() {
+            if !cpython_native_dir.join(PYTHON_EXECUTABLE).exists() {
+                run(Command::new("python3")
+                    .env("WASI_SDK_PATH", WASI_SDK.to_str().unwrap())
+                    .current_dir(&cpython)
+                    .args([
+                        "./Tools/wasm/wasi.py",
+                        "configure-build-python",
+                        "--",
+                        "--config-cache",
+                    ]))
+                .await?;
+
+                run(Command::new("python3")
+                    .env("WASI_SDK_PATH", WASI_SDK.to_str().unwrap())
+                    .current_dir(&cpython)
+                    .args(["./Tools/wasm/wasi.py", "make-build-python"]))
+                .await?;
+            }
+
+            run(Command::new("python3")
+                .env("WASI_SDK_PATH", WASI_SDK.to_str().unwrap())
+                .current_dir(&cpython)
+                .args([
+                    "./Tools/wasm/wasi.py",
+                    "configure-host",
+                    &format!("--host-triple={HOST_TRIPLE}"),
+                    // Current script doesn't work for some reason...
+                    "--host-runner=echo",
+                    "--",
+                    "--config-cache",
+                    &format!("--prefix={}/install", cpython_wasi_dir.to_str().unwrap()),
+                    "--enable-wasm-dynamic-linking",
+                    "--enable-ipv6",
+                    "--disable-test-modules",
+                ]))
+            .await?;
+
+            run(Command::new("python3")
+                .env("WASI_SDK_PATH", WASI_SDK.to_str().unwrap())
+                .current_dir(&cpython)
+                .args([
+                    "./Tools/wasm/wasi.py",
+                    "make-host",
+                    &format!("--host-triple={HOST_TRIPLE}"),
+                ]))
+            .await?;
+
+            run(Command::new("make")
+                .current_dir(&cpython_wasi_dir)
+                .arg("install"))
+            .await?;
+        }
+
+        Ok(())
+    }
+
+    async fn download_and_compile_legacy(self) -> anyhow::Result<()> {
         #[cfg(any(target_os = "macos", target_os = "windows"))]
         const PYTHON_EXECUTABLE: &str = "python.exe";
         #[cfg(not(any(target_os = "macos", target_os = "windows")))]
@@ -84,11 +205,7 @@ impl PythonVersion {
 
         let cpython_wasi_dir = cpython.join("builddir/wasi");
         let cpython_native_dir = cpython.join("builddir/build");
-        if !cpython_wasi_dir
-            .join(format!("libpython{self}.so"))
-            .exists()
-            && !cpython_wasi_dir.join(format!("libpython{self}.a")).exists()
-        {
+        if !cpython_wasi_dir.join(format!("libpython{self}.a")).exists() {
             if !cpython_native_dir.join(PYTHON_EXECUTABLE).exists() {
                 fs::create_dir_all(&cpython_native_dir).await?;
                 fs::create_dir_all(&cpython_wasi_dir).await?;
