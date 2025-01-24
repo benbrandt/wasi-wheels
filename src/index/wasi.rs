de091f659d3c73ca1024ac3b01effa053f6ea7ed
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-01-24 21:52:34 +0100

    test: load and parse all tags

--- src/index/wasi.rs
+++ src/index/wasi.rs
@@ -2,6 +2,11 @@
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
+    use futures_util::TryStreamExt;
+    use tokio::pin;
+
     #[tokio::test]
     async fn get_releases() -> anyhow::Result<()> {
         let github = octocrab::instance();
@@ -9,13 +14,21 @@ mod tests {
             .repos("benbrandt", "wasi-wheels")
             .releases()
             .list()
-            .per_page(1)
             .send()
-            .await?;
+            .await?
+            .into_stream(&github);
+        pin!(releases);
 
-        for release in releases {
-            dbg!(release);
+        let mut packages = HashSet::new();
+
+        while let Some(release) = releases.try_next().await? {
+            let Some((package, _)) = release.tag_name.split_once("/v") else {
+                continue;
+            };
+            packages.insert(package.to_owned());
         }
+
+        dbg!(packages);
 
         Ok(())
     }
